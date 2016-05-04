8cc78f7a15ee843f1bc607f27c1d27d1c966b6a8
rabinovich <mr608@cam.ac.uk>
2016-05-04 14:59:25 -0700

    Miscellaneous

--- LS/problem_generator.py
+++ LS/problem_generator.py
@@ -111,32 +111,59 @@ def randu(entry):
     return rd.uniform(-1,1)
     
 
-def gen_huge_regular(num_datapoints, num_parameters, sparsity, structure='superregular'):
+def gen_huge_regular(num_datapoints, num_parameters, sparsity, 
+                     structure='superregular', p_cross = 0.1,
+                     shuffle_data=False, shuffle_params=False):
     degree = int(sparsity * num_parameters)
+    degree_cross = int(p_cross * sparsity * num_parameters)
     nnz = degree * num_datapoints
 
     print str(degree)
 
-    filestem = "test_instance_huge-superregular_n=%d_d=%d_s=%f.prob" % (num_datapoints, num_parameters, sparsity)
-
-    f = open(filestem, 'w')
-
+    filestem = "test_instance_structure=%s_n=%d_d=%d_s=%f" % (structure, num_datapoints, num_parameters, sparsity)
+    if structure == 'blockmodel': filestem = filestem + ("_pcross=%f" % p_cross)
+    
+    f = open(filestem + ".prob", 'w')
     f.write(str(num_datapoints) + ' ' + str(num_parameters) + ' ' + str(nnz) +  '\n')
 
-    index_permutation = np.random.permutation(num_datapoints)
-    dimension_permutation = np.random.permutation(num_parameters)
-    for i in index_permutation:
-
-        f.write('%f %d ' % (rd.uniform(-1,1), degree))
+    index_permutation = (
+        np.random.permutation(num_datapoints) if shuffle_data else np.arange(num_datapoints)
+    )
     
-        A = np.random.uniform(-1, 1, degree)
-        
-	if structure == 'superregular':
-	     nnz_indices = dimension_permutation[(np.arange(degree) + int(i / degree)*degree) % num_parameters]
-	else:
-	     nnz_indices = np.random.randint(0, num_parameters, degree)
+    dimension_permutation = (
+        np.random.permutation(num_parameters) if shuffle_params else np.arange(num_parameters)
+    )
+    
+    param_counts = np.ones(num_parameters, dtype = int)
+    
+    for i in index_permutation:        
+        if structure == 'superregular':
+            nnz_indices = dimension_permutation[(np.arange(degree) + int(i / degree)*degree) % num_parameters]
+        elif structure == 'regular':
+            # no need to shuffle dimensions
+            nnz_indices = np.random.choice(np.arange(0, num_parameters), size=degree, replace=False)
+        elif structure == 'uniform':
+            # no need to shuffle dimensions
+            rand_degree = np.random.binomial(num_parameters, sparsity)
+            nnz_indices = np.random.choice(np.arange(0, num_parameters), size=rand_degree,replace=False)
+        elif structure == 'blockmodel':
+            offset = int(i / degree)*degree
+            block_params = (np.arange(degree) + offset) % num_parameters
+            outside_block_params = range(0, offset)
+            if (offset + degree < num_parameters): outside_block_params.extend(range(offset + degree, num_parameters))
+            outside_block_params = np.array(outside_block_params, dtype = int)
+            
+            cross_params = np.random.choice(outside_block_params, size=degree_cross,replace=False)
+            nnz_indices = dimension_permutation[np.concatenate([block_params, cross_params])]
+        elif structure == 'pref-attach':
+            nnz_indices = sample_preferential_attachment(degree, param_counts)
 
-        f.write(make_sparse_stringII(nnz_indices, A))
+        total_degree = len(nnz_indices)    
+                        
+        f.write('%f %d ' % (rd.uniform(-1,1), total_degree))
+        Ai = np.random.uniform(-1, 1, total_degree)
+                
+        f.write(make_sparse_stringII(nnz_indices, Ai))
         f.write('\n')
     
         if (i + 1) % 1000 == 0:
@@ -144,7 +171,14 @@ def gen_huge_regular(num_datapoints, num_parameters, sparsity, structure='superr
 
     f.close()
     return 
-
+    
+def sample_preferential_attachment(degree, curr_counts):
+    neighbors = np.random.choice(range(len(curr_counts)), size=degree, replace=False,
+                                 p=np.array(curr_counts, dtype=float) / np.sum(curr_counts))
+    for neigh in neighbors: curr_counts[neigh] += 1
+    
+    return sorted(neighbors)
+    
 def make_sparse_stringII(nnz_indices, row):
     def make_pair(i):
         return '%d %g' % (nnz_indices[i], row[i])
@@ -159,21 +193,9 @@ def main(argv):
     s = float(argv[2])
     structure = argv[3]
     format = argv[4]
+    p_cross = float(argv[5]) if structure == 'blockmodel' else 0.0
 
-
-    if structure == 'huge-regular':
-        return gen_huge_regular(n, d, s)
-
-
-    (X, y) = generate_problem(n, d, s, randu, structure)
-
-
-
-    
-    filestem = "test_instance_superregular_n=%d_d=%d_s=%f" % (n, d, s)
-    problem_tofile(X, y, filestem + ".prob", format)
-
-#    result_tofile(X, y, filestem + ".res")
+    gen_huge_regular(n, d, s, structure, p_cross = p_cross)
 
     return
     
