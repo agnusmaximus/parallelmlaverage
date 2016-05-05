71b67d3200a8d3dae3b81db72229dcb8cecd5dff
rabinovich <mr608@cam.ac.uk>
2016-05-04 20:25:12 -0700

    Sped up corpus generation (maybe?)

--- LS/problem_generator.py
+++ LS/problem_generator.py
@@ -34,7 +34,7 @@ def generate_problem(num_datapoints, num_parameters, sparsity, randomness, struc
         f = np.vectorize(randomness)
         for i in range(num_datapoints):
             if (i + 1) % 1000 == 0:
-                print 'Generated %d data points' % (i+1) 
+                print 'Generated %d data points' % (i+1)
   
             indices = (np.arange(degree) + int(i / degree)*degree) % num_parameters
             A[i,indices] = f(A[i,indices])
@@ -120,7 +120,7 @@ def gen_huge_regular(num_datapoints, num_parameters, sparsity,
 
     print str(degree)
 
-    filestem = "test_instance_structure=%s_n=%d_d=%d_s=%f" % (structure, num_datapoints, num_parameters, sparsity)
+    filestem = "test_instance_faster_structure=%s_n=%d_d=%d_s=%f" % (structure, num_datapoints, num_parameters, sparsity)
     if structure == 'blockmodel': filestem = filestem + ("_pcross=%f" % p_cross)
     
     f = open(filestem + ".prob", 'w')
@@ -136,24 +136,30 @@ def gen_huge_regular(num_datapoints, num_parameters, sparsity,
     
     param_counts = np.ones(num_parameters, dtype = int)
     
-    for i in index_permutation:        
+    for num_done, i in enumerate(index_permutation):        
         if structure == 'superregular':
             nnz_indices = dimension_permutation[(np.arange(degree) + int(i / degree)*degree) % num_parameters]
         elif structure == 'regular':
             # no need to shuffle dimensions
-            nnz_indices = np.random.choice(np.arange(0, num_parameters), size=degree, replace=False)
+            #nnz_indices = np.random.choice(np.arange(0, num_parameters), size=degree, replace=False)
+            nnz_indices = np.random.randint(num_parameters, size = degree)
         elif structure == 'uniform':
             # no need to shuffle dimensions
             rand_degree = np.random.binomial(num_parameters, sparsity)
-            nnz_indices = np.random.choice(np.arange(0, num_parameters), size=rand_degree,replace=False)
+            #nnz_indices = np.random.choice(np.arange(0, num_parameters), size=rand_degree,replace=False)
+            np.random.randint(num_parameters, size=rand_degree)
         elif structure == 'blockmodel':
             offset = int(i / degree)*degree
             block_params = (np.arange(degree) + offset) % num_parameters
-            outside_block_params = range(0, offset)
-            if (offset + degree < num_parameters): outside_block_params.extend(range(offset + degree, num_parameters))
-            outside_block_params = np.array(outside_block_params, dtype = int)
+            #outside_block_params = range(0, offset)
+            #if (offset + degree < num_parameters): outside_block_params.extend(range(offset + degree, num_parameters))
+            #outside_block_params = np.array(outside_block_params, dtype = int)
             
-            cross_params = np.random.choice(outside_block_params, size=degree_cross,replace=False)
+            #cross_params = np.random.choice(outside_block_params, size=degree_cross,replace=False)
+            
+            # Faster way
+            cross_params = np.random.randint(num_parameters, size = degree_cross)
+            
             nnz_indices = dimension_permutation[np.concatenate([block_params, cross_params])]
         elif structure == 'pref-attach':
             nnz_indices = sample_preferential_attachment(degree, param_counts)
@@ -166,8 +172,8 @@ def gen_huge_regular(num_datapoints, num_parameters, sparsity,
         f.write(make_sparse_stringII(nnz_indices, Ai))
         f.write('\n')
     
-        if (i + 1) % 1000 == 0:
-            print 'Wrote to file %d data points' % (i + 1)
+        if (num_done + 1) % 1000 == 0:
+            print 'Wrote to file %d data points' % (num_done + 1)
 
     f.close()
     return 
@@ -175,9 +181,11 @@ def gen_huge_regular(num_datapoints, num_parameters, sparsity,
 def sample_preferential_attachment(degree, curr_counts):
     neighbors = np.random.choice(range(len(curr_counts)), size=degree, replace=False,
                                  p=np.array(curr_counts, dtype=float) / np.sum(curr_counts))
+                                 
+                                                             
     for neigh in neighbors: curr_counts[neigh] += 1
     
-    return sorted(neighbors)
+    return neighbors
     
 def make_sparse_stringII(nnz_indices, row):
     def make_pair(i):
